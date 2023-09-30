c8a0504ad13c47570829342ec736e9eec2f5746f
신 우현 <woo_hyun_1@naver.com>
2023-09-30 09:53:24 +0900

    [design] Add shadow css at auth page

--- src/pages/auth/main/AuthMain.tsx
+++ src/pages/auth/main/AuthMain.tsx
@@ -20,6 +20,7 @@ const AuthMain = () => {
           width: "369px",
           borderRadius: "12px",
           padding: "32px",
+          boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
         }}
       >
         <TopArea>
