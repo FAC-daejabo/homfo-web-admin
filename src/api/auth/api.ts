ac73d94e5f3586cc346af550baba7172f7abd268
신 우현 <woo_hyun_1@naver.com>
2023-11-12 12:25:52 +0900

    [feat] Add approve signup function

--- src/api/auth/api.ts
+++ src/api/auth/api.ts
@@ -1,1 +1,52 @@
+import Swal from "sweetalert2";
+import { IUser } from "../../interfaces/UserInterface";
 import instance from "../util/instance";
+
+export const getUserList = async (
+  setUserList: React.Dispatch<React.SetStateAction<IUser[]>>
+) => {
+  try {
+    const response = await instance.get("admins/users/sign-up/approve");
+    console.log(response.data.data);
+    setUserList(response.data.data);
+  } catch (e: any) {
+    Swal.fire({
+      icon: "error",
+      text: e.response.data.message,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+};
+
+export const approveSignup = async (userId: number, nickName: string) => {
+  try {
+    const result = await Swal.fire({
+      text: `${nickName}님을 승인하시겠습니까?`,
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "예",
+      denyButtonText: "아니오",
+    });
+
+    if (result.isConfirmed) {
+      const response = await instance.patch("admins/users/sign-up/approve", {
+        userId: userId,
+      });
+
+      console.log(response);
+
+      Swal.fire({
+        text: "승인이 완료되었습니다.",
+      });
+    }
+  } catch (e: any) {
+    Swal.fire({
+      icon: "error",
+      text: e.response.data.message,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+};
--- src/pages/admin/AdminUserManagement.tsx
+++ src/pages/admin/AdminUserManagement.tsx
@@ -8,59 +8,13 @@ import {
   PageHeaderWrapper,
   PageTitle,
 } from "../../styles/pages/admin/UserManagement.style";
-
-// const dummyData: IUser[] = [
-//   {
-//     userId: 1,
-//     userAccount: "test1",
-//     userPhoneNum: "01042123964",
-//     nickName: "itsjiny",
-//     gender: "W",
-//     job: "student",
-//     dateOfBirth: "2000-09-19",
-//     createdAt: "2019-09-19 11:45:12",
-//     updatedAt: "2019-09-19 11:45:12",
-//     role: "관리자",
-//     status: "P",
-//   },
-//   {
-//     userId: 2,
-//     userAccount: "test2",
-//     userPhoneNum: "01042123964",
-//     nickName: "king",
-//     gender: "M",
-//     job: "student",
-//     dateOfBirth: "2000-09-19",
-//     createdAt: "2019-09-19 11:45:12",
-//     updatedAt: "2019-09-19 11:45:12",
-//     role: "일반회원",
-//     status: "P",
-//   },
-//   {
-//     userId: 3,
-//     userAccount: "test3",
-//     userPhoneNum: "01042123964",
-//     nickName: "sung",
-//     gender: "M",
-//     job: "student",
-//     dateOfBirth: "2000-09-19",
-//     createdAt: "2019-09-19 11:45:12",
-//     updatedAt: "2019-09-19 11:45:12",
-//     role: "관리자",
-//     status: "P",
-//   },
-// ];
+import { getUserList } from "../../api/auth/api";
 
 const AdminUserManagement = () => {
   const [userList, setUserList] = useState<IUser[]>([]);
-  const getUserList = async () => {
-    const response = await instance.get("admins/users/sign-up/approve");
-    console.log(response.data.data);
-    setUserList(response.data.data);
-  };
 
   useEffect(() => {
-    getUserList();
+    getUserList(setUserList);
   }, []);
   return (
     <>
